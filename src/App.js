4538e02d1916fcbe9a7d058fe5559ee48402a7cb
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-12-27 04:52:03 +0530

    layout ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Grid, Typography } from '@material-ui/core';
+import { Container, Grid, Typography, AppBar, Toolbar, IconButton } from '@material-ui/core';
+import PublicIcon from '@material-ui/icons/Public';
 import Cardblock from './Cardblock/CardBlock';
 
 import './App.css';
@@ -7,21 +8,26 @@ import './App.css';
 function App() {
 	return (
 		<Container>
-			<header>
-				<Typography variant={'h3'}>Important WEB Links</Typography>
-				<Typography variant={'caption'}>Links that make web development easy</Typography>
-			</header>
-			<main>
+			<AppBar position="fixed">			
+				<Toolbar variant="dense" className="jc-center site-header">
+					<IconButton edge="start" color="inherit" aria-label="Useful web links">
+						<PublicIcon />
+					</IconButton>
+					<Typography variant={'h5'}>Useful WEB Links</Typography>
+					<IconButton edge="end" color="inherit" aria-label="Useful web links">
+						<PublicIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+			<main className="main-block">
 				<Grid container spacing={3}>
+					<Typography variant={'subtitle2'} className="site-note">
+						Info: This site content will be updated time to time. All links are owned by individual site owners and this
+						site don't hold the responsibility for any changes in the given links.
+					</Typography>
 					<Cardblock />
 				</Grid>
 			</main>
-			<footer>
-				<Typography variant={'h6'}>
-					Note: These links will be updated time to time. All links are owned by individual owners and this
-					site don't hold the responsibility for any changes in the given links.
-				</Typography>
-			</footer>
 		</Container>
 	);
 }
--- src/Cardblock/CardBlock.js
+++ src/Cardblock/CardBlock.js
@@ -1,41 +1,62 @@
-import React from "react";
+import React from 'react';
 import { Typography, Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
 const data = [
-    {
-        name: "Cheatsheet for regular expression",
-        websitelink: "https://ihateregex.io/"
-    },
-    {
-        name: "Beautiful, free illustrations",
-        websitelink: "https://www.drawkit.io/"
-    },
-    {
-        name: "Find free fonts",
-        websitelink: "https://fontflipper.com/"
-    },
-    {
-        name: "Expose localhost to the internet with public url",
-        websitelink: "https://ngrok.com/"
-    },
-    {
-        name: "Free to use fake online REST API for testing and prototyping",
-        websitelink: "http://jsonplaceholder.typicode.com/"
-    }
+	{
+		name: 'Cheatsheet for regular expression',
+		websitelink: 'https://ihateregex.io/'
+	},
+	{
+		name: 'Beautiful, free illustrations',
+		websitelink: 'https://www.drawkit.io/'
+	},
+	{
+		name: 'Find free fonts',
+		websitelink: 'https://fontflipper.com/'
+	},
+	{
+		name: 'Expose localhost to the internet with public url',
+		websitelink: 'https://ngrok.com/'
+	},
+	{
+		name: 'Free to use fake online REST API for testing and prototyping',
+		websitelink: 'http://jsonplaceholder.typicode.com/'
+	}
 ];
 
-export default function CardBlock() {
-    return (
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: 'center',
+        color: "#F6F6F6",
+        backgroundColor: "#212121",
+		transition: "0.5s",
 
-        <Grid item xs={12} sm={6} className="card-block">
-            {
-                data.map((item) =>
-                <div className="card-block-item" key={`${item.name}-key`}>
-                    <Typography variant={"h6"}>{item.name}</Typography>
-                    <a href={item.websitelink} target="_blank" rel="noreferrer">{item.websitelink}</a>
-                </div> 
-                )
-            }
-        </Grid>
-    );
+        "&:hover": {
+            backgroundColor: "#383838",   
+        }
+	}
+}));
+
+export default function CardBlock() {
+    const classes = useStyles();
+	return (
+		<React.Fragment>
+			{data.map((item) => (
+				<Grid item xs={12} sm={4} key={`${item.name}-key`}>
+					<Paper className={classes.paper}>
+						<Typography variant={'h6'}>{item.name}</Typography>
+						<a href={item.websitelink} target="_blank" rel="noreferrer">
+							{item.websitelink}
+						</a>
+					</Paper>
+				</Grid>
+			))}
+		</React.Fragment>
+	);
 }
